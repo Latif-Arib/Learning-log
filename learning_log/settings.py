ece0dfb0171a6b502cb9a229edc21a0fcfce83cd
Latif-Arib <labdul749@gmail.com>
2023-08-22 08:16:46 +0530

    Database changed from sqlite3 to mysql

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learning_log',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Change this to your MySQL host
+        'PORT': '3306',       # Change this to your MySQL port
     }
 }
 
